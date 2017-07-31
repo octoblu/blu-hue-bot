c81405c219890b2719c6a3c17b33dabafff9906a
weirdestnerd <olu1995@hotmail.com>
2017-07-30 18:59:51 -0700

    Script to read env var from .env

---
+++ env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script reads .env file for variables and exports the variables to the environment
+file=.env
+echo "Reading .env file"
+while read line; do
+  echo "Exporting: " $line
+  export $line;
+done < $file
+echo "Successfully exported each line"
